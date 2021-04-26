e5f374fe96a824061a1ce695419007e9cc93116c
tunngnt <tungnt@icd-vn.com>
2021-04-26 14:58:37 +0700

    tungnt

---
+++ components/layout/layout.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+
+const MasterLayout = (props) => (
+  <>
+    <Head>
+      <title>Default</title>
+      <meta charSet="utf-8" />
+    </Head>
+  </>
+);
+export default MasterLayout;
