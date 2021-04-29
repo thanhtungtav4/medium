9a380e5214175e5df3c76ce0d4914c877db58c83
tunngnt <tungnt@icd-vn.com>
2021-04-29 13:25:17 +0700

    tungnt

---
+++ components/module/Featured.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+export default class Featured extends Component {  
+  render() {
+  return (
+    <>
+            <div className="col-sm-12 col-md-9 col-xl-9">
+          <h2 className="spanborder h4">
+            <span>Editor's Picks</span>
+          </h2>
+          <div className="row">
+            <div className="col-sm-12 col-md-6">
+              <article className="first mb-3">
+                <figure><a href="single.html"><img src="static/images/thumb/thumb-1240x700.jpg" alt={true} /></a></figure>
+                <h3 className="entry-title mb-3"><a href="single.html">Home Internet Is Becoming a Luxury for the Wealthy</a></h3>
+                <div className="entry-excerpt">
+                  <p>
+                    And black on meretriciously regardless well fearless irksomely as about hideous wistful bat less oh much and occasional useful rat darn jeepers far. 
+                  </p>
+                </div>
+                <div className="entry-meta align-items-center">
+                  <a href="author.html">Dave Gershgorn</a> in <a href="archive.html">OneZero</a><br />                                  
+                  <span>Jun 14</span>
+                  <span className="middotDivider" />
+                  <span className="readingTime" title="3 min read">3 min read</span>
+                  <span className="svgIcon svgIcon--star">
+                    <svg className="svgIcon-use" width={15} height={15}>
+                      <path d="M7.438 2.324c.034-.099.09-.099.123 0l1.2 3.53a.29.29 0 0 0 .26.19h3.884c.11 0 .127.049.038.111L9.8 8.327a.271.271 0 0 0-.099.291l1.2 3.53c.034.1-.011.131-.098.069l-3.142-2.18a.303.303 0 0 0-.32 0l-3.145 2.182c-.087.06-.132.03-.099-.068l1.2-3.53a.271.271 0 0 0-.098-.292L2.056 6.146c-.087-.06-.071-.112.038-.112h3.884a.29.29 0 0 0 .26-.19l1.2-3.52z" />
+                    </svg>
+                  </span>
+                </div>
+              </article>
+              <a className="btn btn-green d-inline-block mb-4 mb-md-0" href="archive.html">All Featured</a>
+            </div>
+            <div className="col-sm-12 col-md-6">
+              <article className="post-has-bg">
+                <div className="mb-3 d-flex row">
+                  <figure className="col-4 col-md-4"><a href="single.html"><img src="static/images/thumb/thumb-700x512.jpg" alt={true} /></a></figure>
+                  <div className="entry-content col-8 col-md-8 pl-md-0">                                                    
+                    <h5 className="entry-title mb-3"><a href="single.html">The Night My Doorbell Camera Captured a Shooting</a></h5>
+                    <div className="entry-meta align-items-center">
+                      <a href="author.html">Alentica</a> in <a href="archive.html">Police</a><br />
+                      <span>Jun 16</span>
+                      <span className="middotDivider" />
+                      <span className="readingTime" title="3 min read">7 min read</span>
+                      <span className="svgIcon svgIcon--star">
+                        <svg className="svgIcon-use" width={15} height={15}>
+                          <path d="M7.438 2.324c.034-.099.09-.099.123 0l1.2 3.53a.29.29 0 0 0 .26.19h3.884c.11 0 .127.049.038.111L9.8 8.327a.271.271 0 0 0-.099.291l1.2 3.53c.034.1-.011.131-.098.069l-3.142-2.18a.303.303 0 0 0-.32 0l-3.145 2.182c-.087.06-.132.03-.099-.068l1.2-3.53a.271.271 0 0 0-.098-.292L2.056 6.146c-.087-.06-.071-.112.038-.112h3.884a.29.29 0 0 0 .26-.19l1.2-3.52z" />
+                        </svg>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </article>
+              <article>
+                <div className="mb-3 d-flex row">
+                  <figure className="col-4 col-md-4"><a href="single.html"><img src="static/images/thumb/thumb-512x512-2.jpg" alt={true} /></a></figure>
+                  <div className="entry-content col-8 col-md-8 pl-md-0">
+                    <h5 className="entry-title mb-3"><a href="single.html">Privacy Is Just the Beginning of the Debate Over Tech</a></h5>
+                    <div className="entry-meta align-items-center">
+                      <a href="author.html">Otimus</a> in <a href="archive.html">Startup</a><br />                                    
+                      <span>May 15</span>
+                      <span className="middotDivider" />
+                      <span className="readingTime" title="3 min read">6 min read</span>
+                      <span className="svgIcon svgIcon--star">
+                        <svg className="svgIcon-use" width={15} height={15}>
+                          <path d="M7.438 2.324c.034-.099.09-.099.123 0l1.2 3.53a.29.29 0 0 0 .26.19h3.884c.11 0 .127.049.038.111L9.8 8.327a.271.271 0 0 0-.099.291l1.2 3.53c.034.1-.011.131-.098.069l-3.142-2.18a.303.303 0 0 0-.32 0l-3.145 2.182c-.087.06-.132.03-.099-.068l1.2-3.53a.271.271 0 0 0-.098-.292L2.056 6.146c-.087-.06-.071-.112.038-.112h3.884a.29.29 0 0 0 .26-.19l1.2-3.52z" />
+                        </svg>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </article>
+              <article>
+                <div className="mb-3 d-flex row">
+                  <figure className="col-4 col-md-4"><a href="single.html"><img src="static/images/thumb/thumb-700x512-2.jpg" alt={true} /></a></figure>
+                  <div className="entry-content col-8 col-md-8 pl-md-0">
+                    <div className="capsSubtle mb-2">based on your reading history</div>
+                    <h5 className="entry-title mb-3"><a href="single.html">Want To Make Millions? Then Act Like a Millionaire</a></h5>
+                    <div className="entry-meta align-items-center">
+                      <a href="author.html">Mark Harris</a> in <a href="archive.html">Heated</a><br />                                    
+                      <span>May 13</span>
+                      <span className="middotDivider" />
+                      <span className="readingTime" title="3 min read">12 min read</span>
+                      <span className="svgIcon svgIcon--star">
+                        <svg className="svgIcon-use" width={15} height={15}>
+                          <path d="M7.438 2.324c.034-.099.09-.099.123 0l1.2 3.53a.29.29 0 0 0 .26.19h3.884c.11 0 .127.049.038.111L9.8 8.327a.271.271 0 0 0-.099.291l1.2 3.53c.034.1-.011.131-.098.069l-3.142-2.18a.303.303 0 0 0-.32 0l-3.145 2.182c-.087.06-.132.03-.099-.068l1.2-3.53a.271.271 0 0 0-.098-.292L2.056 6.146c-.087-.06-.071-.112.038-.112h3.884a.29.29 0 0 0 .26-.19l1.2-3.52z" />
+                        </svg>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
+          </div>                
+        </div>{/*end featured*/}                
+        {/*begin Trending*/}
+    </>
+  );
+}
+}
--- pages/categories.js
+++ pages/categories.js
@@ -1,26 +1,25 @@
-import { useEffect, useState } from "react";
+import React from "react";
+import StickyBox from "react-sticky-box";
 import Head from "next/head";
-import { useTheme } from "next-themes";
-export default function Home() {
-  const [isMounted, setIsMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
-useEffect(() => {
-    setIsMounted(true);
-  }, []);
-const switchTheme = () => {
-    if (isMounted) {
-      setTheme(theme === "light" ? "dark" : "light");
-    }
-  };
-return (
-    <div className="text-center">
-      <Head>
-      
-      </Head>
-      <h1 className="text:2xl">
-       Dark mode with Tailwind and Next- themes
-      </h1>
-      <button onClick={switchTheme}>Change theme</button>
-    </div>
-  );
-}
+import Layout from "../components/layout/layout";
+const Categories = () => (
+  <>
+   <Head>
+      <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Categories</title>
+        <meta name="description" content='{meta.description}'/>
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content='{meta.image}' />
+        <meta name="keywords" content='{meta.keywords}'></meta>
+        <link href="https://fonts.googleapis.com/css?family=Cabin:400,700&display=swap" rel="stylesheet" /> 
+        <link href="https://fonts.googleapis.com/css?family=B612+Mono&display=swap" rel="stylesheet" /> 
+        <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet" />
+  </Head>
+  <Layout>
+
+  </Layout>
+ 
+</>
+);
+export default Categories;
