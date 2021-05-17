a4b23ae9cddb878b48df72332ea86740f2ddeb24
tunngnt <tungnt@icd-vn.com>
2021-05-17 17:02:52 +0700

    tungnt

--- pages/[slug].js
+++ pages/[slug].js
@@ -64,7 +64,7 @@ export default class PostDetail extends Component {
     .catch(err =>console.log(err));
   }
   render() {
-    var PostData = this.state.DataAPi[0];
+    var PostData = this.state.DataAPi;
     return (
       <>
         <Head>
